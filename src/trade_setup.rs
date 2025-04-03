2810d8c97cac208c06f45ddc89aaf15458bac9a5
0xNoSystem <joesawayajsx@gmail.com>
2025-04-03 12:42:54 +0200

    kenfi

--- src/trade_setup.rs
+++ src/trade_setup.rs
@@ -1,6 +1,10 @@
 use hyperliquid_rust_sdk::{ExchangeClient};
+//use kwant::indicators::{Rsi, StochRsi, Atr, Adx, Ema, EmaCross, Sma};
 use log::info;
 use std::fmt;
+
+
+
 
 #[derive(Clone, Debug, Copy)]
 pub enum Risk {
@@ -10,18 +14,86 @@ pub enum Risk {
 }
 
 #[derive(Clone, Debug, Copy)]
-pub enum Strategy {
+pub enum Style{
+    Scalp,
+    Swing,
+}
+
+#[derive(Clone, Debug, Copy)]
+pub enum Direction{
     Bull,
     Bear,
     Neutral,
 }
 
 
+#[derive(Clone, Debug, Copy)]
+pub struct Strategy {
+   pub risk: Risk,
+   pub style: Style,    
+   pub direction: Direction,
+   pub follow_trend: bool,
+}
+
+
+
+
+
+
+
+impl Strategy{
+
+    pub fn new(risk: Risk, style: Style, trend: Direction, follow_trend: bool) -> Self{
+        Self { risk, style, trend, follow_trend }
+    }
+
+    
+    pub fn get_rsi_threshold(&self) -> (f32, f32){
+        match self.risk{
+            Risk::Low => (25.0, 78.0),
+            Risk::Medium => (30.0, 70.0),
+            Risk::High => (33.0, 68.0),
+        }
+    }
+
+
+
+
+    pub fn update_risk(&mut self, risk: Risk){
+        self.risk = risk;
+    }
+
+    pub fn update_style(&mut self, style: Style){
+        self.style = style;
+    }
+
+    pub fn update_direction(&mut self, direction: Direction){
+        self.direction = direction;
+    }
+    
+    pub fn update_follow_trend(&mut self, follow_trend: bool){
+        self.follow_trend = follow_trend;
+    }
+}
+
+
+impl Default for Strategy{
+    fn default() -> Self {
+        Self { risk: Risk::Medium, style: Style::Scalp, direction: Direction::Neutral, follow_trend: true }
+    }
+}
+
+
+
+
+
+
+
+
 
 #[derive(Clone, Debug)]
 pub struct TradeParams {
-    pub strategy: Strategy,
-    pub risk: Risk,
+    pub strategy: Strategy, 
     pub lev: u32,
     pub trade_time: u64, // in seconds
     pub asset: String,
