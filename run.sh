2b735614555855adc03203c6d436f3e05c4aba9e
0xNoSystem <joesawayajsx@gmail.com>
2025-11-04 10:42:56 +0200

    mv src/frontend/interface ./web_ui

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 cargo run --release --bin load #update tradable assets list
 
-
 RUST_LOG=info cargo run --release --bin kwant & #PREFIX WITH "RUST_LOG=info" for logging
 
-cd ./src/frontend/interface
+cd ./web_ui
 
 npm install
 npm run dev
